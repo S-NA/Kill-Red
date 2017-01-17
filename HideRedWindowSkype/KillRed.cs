2ef1d0110c8dcc9fa4e2979ca75851ee52280d1b
S. <S-NA@users.noreply.github.com>
2017-01-17 14:04:05 -0500

    Clean-up usings, and add auto-detect of red border.

--- HideRedWindowSkype/KillRed.cs
+++ HideRedWindowSkype/KillRed.cs
@@ -6,14 +6,26 @@ namespace HideRedWindowSkype
 {
     public partial class KillRed : Form
     {
+        private bool hidden = false;
         public KillRed()
         {
             InitializeComponent();
+            hidden = false;
+            if (WndMgmt.NativeMethods.FindWindow("TSimpleSelection", "") == IntPtr.Zero)
+            {
+                MessageBox.Show("No red window found.", "Error");
+            }
+            else
+            {
+                WndMgmt.wHide();
+                hidden = true;
+                checkBox1.Checked = true;
+            }
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-            bool hidden = checkBox1.Checked;
+            hidden = checkBox1.Checked;
             if (hidden)
             {
                 WndMgmt.wHide();
